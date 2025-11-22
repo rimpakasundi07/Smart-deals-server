cb5d0d41a02fbe8f84e7a7a1dd10d1c5483d2cd1
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-22 19:41:25 +0530

    Add MongoDB product CRUD API

--- index.js
+++ index.js
@@ -48,13 +48,22 @@ async function run() {
     const productsCollection = db.collection("products");
 
     app.get("/products", async (req, res) => {
-      const projectFields = { title: 1, price_min: 1, price_max: 1 };
-      const cursor = productsCollection
-        .find()
-        .sort({ price_min: 1 })
-        .skip(3)
-        .limit(5)
-        .project(projectFields);
+      // const projectFields = { title: 1, price_min: 1, price_max: 1, image: 1 };
+      // const cursor = productsCollection
+      //   .find()
+      //   .sort({ price_min: 1 })
+      //   .skip(3)
+      //   .limit(5)
+      //   .project(projectFields);
+
+      console.log(req.query);
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+
+      const cursor = productsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
