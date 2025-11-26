ee9160c661219a835b952ff6794e1c3d4443c5d3
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-26 21:40:57 +0530

    feat: add Express server with MongoDB, products, bids, and users API

---
+++ __jwt__.js
@@ -0,0 +1,14 @@
+/**
+ *  1. After login : server will create a jwt token
+ *  2. Stroe it in the client side(local storage, http only cookies , in memory)
+ *  3. For asking for sensitive data: send a request with jwt token in the header
+ *  4. Server will verify the token . If token is valid ; then will provide the data
+ *
+ *
+ *
+ *  ---------------------- ACCESS TOKEN REFRESH TOKEN--------------------------------------------
+ *
+ *
+ *
+ *
+ */
--- index.js
+++ index.js
@@ -1,178 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// const port = process.env.PORT || 3000;
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // smartdbUser
-// // password: VuqPU32Z4DueSIEu
-// const uri =
-//   "mongodb+srv://smartdbUser:VuqPU32Z4DueSIEu@cluster0.obgikox.mongodb.net/?appName=Cluster0";
-
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// app.get("/", (req, res) => {
-//   res.send("Smart server is running");
-// });
-
-// async function run() {
-//   try {
-//     await client.connect();
-
-//     const db = client.db("smart_db");
-//     const productsCollection = db.collection("products");
-//     const bidsCollection = db.collection("bids");
-//     const usersCollection = db.collection("users");
-
-//     // users API
-
-//     app.post("/users", async (req, res) => {
-//       const newUser = req.body;
-//       const email = req.body.email;
-//       const query = { email: email };
-//       const existingUser = await usersCollection.findOne(query);
-//       if (existingUser) {
-//         res.send({
-//           message: "user already exits. Do not need to insert again ",
-//         });
-//       } else {
-//         const result = await usersCollection.insertOne(newUser);
-//         res.send(result);
-//       }
-//     });
-
-//     // products API
-
-//     app.get("/products", async (req, res) => {
-//       // const projectFields = { title: 1, price_min: 1, price_max: 1, image: 1 };
-//       // const cursor = productsCollection
-//       //   .find()
-//       //   .sort({ price_min: 1 })
-//       //   .skip(3)
-//       //   .limit(5)
-//       //   .project(projectFields);
-
-//       console.log(req.query);
-//       const email = req.query.email;
-//       const query = {};
-//       if (email) {
-//         query.email = email;
-//       }
-
-//       const cursor = productsCollection.find(query);
-//       const result = await cursor.toArray();
-//       res.send(result);
-//     });
-
-//     app.get("/latest-products", async (req, res) => {
-//       const cursor = productsCollection
-//         .find()
-//         .sort({ created_at: -1 })
-//         .limit(9);
-//       const result = await cursor.toArray();
-//       res.send(result);
-//     });
-
-//     app.get("/products/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const result = await productsCollection.findOne(query);
-//       res.send(result);
-//     });
-
-//     app.post("/products", async (req, res) => {
-//       const newProduct = req.body;
-//       const result = await productsCollection.insertOne(newProduct);
-//       res.send(result);
-//     });
-
-//     app.patch("/products/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const updatedProduct = req.body;
-//       const query = { _id: new ObjectId(id) };
-//       const update = {
-//         $set: {
-//           name: updatedProduct.name,
-//           price: updatedProduct.price,
-//         },
-//       };
-//       const result = await productsCollection.updateOne(query, update);
-//       res.send(result);
-//     });
-
-//     app.delete("/products/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const result = await productsCollection.deleteOne(query);
-//       res.send(result);
-//     });
-
-//     // bids related apis
-
-//     app.get("/bids", async (req, res) => {
-//       const email = req.query.email;
-//       const query = {};
-//       if (email) {
-//         query.buyer_email = email;
-//       }
-//       const cursor = bidsCollection.find(query);
-//       const result = await cursor.toArray();
-//       res.send(result);
-//     });
-
-//     app.post("/bids", async (req, res) => {
-//       const newBid = req.body;
-//       const result = await bidsCollection.insertOne(newBid);
-//       res.send(result);
-//     });
-
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//   }
-// }
-// run().catch(console.dir);
-
-// app.listen(port, () => {
-//   console.log(`Smart server is running on port: ${port} `);
-// });
-
-// // client
-// //   .connect()
-// //   .then(() => {
-// //     app.listen(port, () => {
-// //       console.log(`Smart server is running now on port: ${port} `);
-// //     });
-// //   })
-// //   .catch(console.dir);
-
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -185,10 +10,24 @@ console.log(process.env);
 app.use(cors());
 app.use(express.json());
 
+const logger = (req, res, next) => {
+  console.log("logging information");
+  next();
+};
+
+const verifyFireBaseToken = (req, res, next) => {
+  console.log("in the verify middleware", req.headers.authorization);
+  if (!req.headers.authorization) {
+    // do not allow to go
+  }
+
+  //
+  next();
+};
+
 // MongoDB Connection
 
-const uri = `const uri =
-  "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obgikox.mongodb.net/?appName=Cluster0";`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obgikox.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -303,7 +142,8 @@ async function run() {
     // --------------------
     // Bids API
     // --------------------
-    app.get("/bids", async (req, res) => {
+    app.get("/bids", logger, verifyFireBaseToken, async (req, res) => {
+      // console.log("headers", req.headers);
       const email = req.query.email;
       const query = {};
 
