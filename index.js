a846ad7569c11de71b436581e030415f83ab4484
rimpa kasundi <rimpakasundi@gmail.com>
2025-11-27 20:18:25 +0530

    Add Firebase token verification to product bids endpoint

--- index.js
+++ index.js
@@ -181,13 +181,17 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/products/bids/:productId", async (req, res) => {
-      const productId = req.params.productId;
-      const query = { product: productId };
-      const cursor = bidsCollection.find(query).sort({ bid_price: -1 });
-      const result = await cursor.toArray();
-      res.send(result);
-    });
+    app.get(
+      "/products/bids/:productId",
+      verifyFireBaseToken,
+      async (req, res) => {
+        const productId = req.params.productId;
+        const query = { product: productId };
+        const cursor = bidsCollection.find(query).sort({ bid_price: -1 });
+        const result = await cursor.toArray();
+        res.send(result);
+      }
+    );
 
     app.post("/bids", async (req, res) => {
       const newBid = req.body;
